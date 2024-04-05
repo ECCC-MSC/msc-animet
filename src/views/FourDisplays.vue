d7e1b6b2d96d09aae6da51161d77abc0d72ac4e3
Philippe Theroux <therouxp@geomet-dev-22.cmc.ec.gc.ca>
2024-04-05 17:46:06 +0000

    Refresh on non-time enabled, date size increase and 4-displays

---
+++ src/views/FourDisplays.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page">
+    <iframe
+      v-for="n in 4"
+      :key="n"
+      src="https://eccc-msc.github.io/msc-animet/"
+      class="quadrant"
+    ></iframe>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+.quadrant {
+  width: 50%;
+  height: 50%;
+  float: left;
+}
+</style>
